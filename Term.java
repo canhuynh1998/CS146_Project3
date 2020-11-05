5a21867ba07723b07db69428b1b73685d3253e26
Hoc Can Huynh <canhuynh1998@gmail.com>
2020-11-04 17:16:56 -0800

    Term API and BinarySearch

---
+++ Term.java
@@ -0,0 +1,106 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// An immutable data type that represents an autocomplete term: a query string
+// and an associated real-valued weight.
+public class Term implements Comparable<Term> {
+    private String query;
+    private long weight;
+
+    // Construct a term given the associated query string, having weight 0.
+    public Term(String query) {
+        if (query == null) {
+            throw new NullPointerException("The query is empty");
+        }
+        this.query = query;
+        this.weight = 0L;
+    }
+
+    // Construct a term given the associated query string and weight.
+    public Term(String query, long weight) {
+        if (query == null) {
+            throw new NullPointerException("The query is empty");
+        }
+        if (weight < 0) {
+            throw new IllegalArgumentException("Weight is negative");
+        }
+        this.query = query;
+        this.weight = weight;
+    }
+
+    // A reverse-weight comparator.
+    public static Comparator<Term> byReverseWeightOrder() {
+        return new ReverseWeightOrder();
+    }
+
+    // Helper reverse-weight comparator.
+    private static class ReverseWeightOrder implements Comparator<Term> {
+        public int compare(Term v, Term w) {
+            return (int) (w.weight - v.weight);
+        }
+    }
+
+    // A prefix-order comparator.
+    public static Comparator<Term> byPrefixOrder(int r) {
+        if (r < 0) {
+            throw new IllegalArgumentException("Negative length");
+        }
+        return new PrefixOrder(r);
+    }
+
+    // Helper prefix-order comparator.
+    private static class PrefixOrder implements Comparator<Term> {
+        private int range;  //range to compare for each substring
+
+        PrefixOrder(int r) {
+            this.range = r;
+        }
+
+
+        //Comparing each substring of v and w
+        //in the range of [0, range)
+        public int compare(Term v, Term w) {
+            return v.query.substring(0, range).compareTo(w.query.substring(0, range));
+        }
+    }
+
+    // Compare this term to that in lexicographic order by query and
+    // return a negative, zero, or positive integer based on whether this
+    // term is smaller, equal to, or larger than that term.
+    public int compareTo(Term that) {
+        return this.query.compareTo(that.query);
+    }
+
+    // A string representation of this term.
+    public String toString() {
+        return weight + "\t" + query;
+    }
+
+    // Test client. [DO NOT EDIT]
+    public static void main(String[] args) {
+        String filename = args[0];
+        int k = Integer.parseInt(args[1]);
+        In in = new In(filename);
+        int N = in.readInt();
+        Term[] terms = new Term[N];
+        for (int i = 0; i < N; i++) {
+            long weight = in.readLong();
+            in.readChar();
+            String query = in.readLine();
+            terms[i] = new Term(query.trim(), weight);
+        }
+        StdOut.printf("Top %d by lexicographic order:\n", k);
+        Arrays.sort(terms);
+        for (int i = 0; i < k; i++) {
+            StdOut.println(terms[i]);
+        }
+        StdOut.printf("Top %d by reverse-weight order:\n", k);
+        Arrays.sort(terms, Term.byReverseWeightOrder());
+        for (int i = 0; i < k; i++) {
+            StdOut.println(terms[i]);
+        }
+    }
+}
